bbf619a4c6fbf792401895423b71de588aa8bf75
luan <luan640@gmail.com>
2024-09-26 09:38:09 -0300

    Update ALLOWED_HOSTS

--- apontamento_v2/settings.py
+++ apontamento_v2/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-h2z=dv@87s1ypjnh%(oa*6gdcwwofbt(%m1@1zde1x5dfhy2en
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['apontamentousinagem.onrender.com']
 
 # Application definition
 
