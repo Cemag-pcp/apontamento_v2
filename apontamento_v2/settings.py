772313ded75356cc9c03f716036e93623ffbbd4a
luan <luan640@gmail.com>
2024-09-27 10:26:08 -0300

    testando sem postgresql

--- apontamento_v2/settings.py
+++ apontamento_v2/settings.py
@@ -81,27 +81,27 @@ WSGI_APPLICATION = 'apontamento_v2.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': '15512332',
-        'HOST': 'database-1.cdcogkfzajf0.us-east-1.rds.amazonaws.com',
-        'PORT': '5432',
-        'OPTIONS': {
-            'options': '-c search_path=apontamento_v2',
-        },
-
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'postgres',
+#         'USER': 'postgres',
+#         'PASSWORD': '15512332',
+#         'HOST': 'database-1.cdcogkfzajf0.us-east-1.rds.amazonaws.com',
+#         'PORT': '5432',
+#         'OPTIONS': {
+#             'options': '-c search_path=apontamento_v2',
+#         },
+
+#     }
+# }
 
 
 # Password validation
