f7d00d71a62d9286c724723d35fe2b48187a9d32
luan <luan640@gmail.com>
2025-05-02 08:47:37 -0300

    rota

--- apontamento_v2/urls.py
+++ apontamento_v2/urls.py
@@ -2,8 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from core import views as core_views 
 
 urlpatterns = [
+    path('', core_views.home, name='home'), 
+
     path('core/', include('core.urls')),
     path('admin/', admin.site.urls),
 
