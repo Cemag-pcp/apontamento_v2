2a1ebce582a779362cf549ea3591d7cb2f9a6647
luan <luan640@gmail.com>
2025-05-08 13:30:57 -0300

    tempos de montagem

--- apontamento_montagem/views.py
+++ apontamento_montagem/views.py
@@ -777,29 +777,3 @@ def api_ordens_finalizadas(request):
 
     return JsonResponse(data, safe=False)
 
-def api_tempos(request):
-
-    """
-    
-    """
-
-    dados = PecasOrdem.objects.select_related(
-        'ordem',                    # o.*
-        'processo_ordem',           # op.*
-        'ordem__maquina'            # m.*
-    ).filter(
-        ordem__grupo_maquina='montagem',
-        ordem_id=29385
-    ).values(
-        'ordem__id',                         # id_ordem
-        'ordem__ordem',                      # ordem
-        'peca',                              # codigo
-        'peca',                              # descricao (se quiser dividir, posso te ajudar)
-        'processo_ordem__data_inicio',      # data_inicio
-        'processo_ordem__data_fim',         # data_fim
-        'ordem__data_carga',                # data_carga
-        'qtd_planejada',                    # qt_planejada
-        'ordem__maquina__nome',             # celula (m.nome)
-        'processo_ordem__status',           # status
-        'qtd_boa',                          # qt_boa
-    )
