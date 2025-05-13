419bf8adb75dcf4e6e5a006ff40208e4053499dd
SaulSantos1 <saulsantos111@gmail.com>
2025-05-13 14:17:06 -0300

    :bug: fix Trazendo inspetores direto pelo Login

--- inspecao/views.py
+++ inspecao/views.py
@@ -168,10 +168,20 @@ def inspecao_pintura(request):
         {"nome_usuario": user.user.username, "id": user.user.id} for user in users
     ]
 
+    user_profile = Profile.objects.filter(user=request.user).first()
+    if user_profile and user_profile.tipo_acesso == "inspetor" and user_profile.permissoes.filter(nome="inspecao/pintura").exists():
+        inspetor_logado = {
+            "nome_usuario": request.user.username,
+            "id": request.user.id
+        }
+    else:
+        inspetor_logado = None
+
     return render(
         request,
         "inspecao_pintura.html",
-        {"inspetores": lista_inspetores, "causas": list_causas, "cores": cores},
+        {"inspetor_logado":inspetor_logado, "inspetores": lista_inspetores, 
+         "causas": list_causas, "cores": cores},
     )
 
 
@@ -1145,7 +1155,16 @@ def inspecao_estamparia(request):
         for inspetor in inspetores
     ]
 
-    context = {"maquinas": maquinas, "motivos": motivos, "inspetores": inspetores}
+    user_profile = Profile.objects.filter(user=request.user).first()
+    if user_profile and user_profile.tipo_acesso == "inspetor" and user_profile.permissoes.filter(nome="inspecao/estamparia").exists():
+        inspetor_logado = {
+            "nome_usuario": request.user.username,
+            "id": request.user.id
+        }
+    else:
+        inspetor_logado = None
+
+    context = {"maquinas": maquinas, "motivos": motivos, "inspetores": inspetores, "inspetor_logado":inspetor_logado}
 
     return render(request, "inspecao_estamparia.html", context)
 
@@ -1883,10 +1902,20 @@ def inspecao_tanque(request):
         {"peca": f"{tanque.codigo} - {tanque.descricao}"} for tanque in tanques
     ]
 
+    user_profile = Profile.objects.filter(user=request.user).first()
+    if user_profile and user_profile.tipo_acesso == "inspetor" and user_profile.permissoes.filter(nome="inspecao/tanque").exists():
+        inspetor_logado = {
+            "nome_usuario": request.user.username,
+            "id": request.user.id
+        }
+    else:
+        inspetor_logado = None
+
     return render(
         request,
         "inspecao_tanque.html",
-        {"tanques": dict_tanques, "inspetores": lista_inspetores},
+        {"tanques": dict_tanques, "inspetores": lista_inspetores, 
+         "inspetor_logado":inspetor_logado},
     )
 
 
@@ -1917,6 +1946,15 @@ def inspecao_tubos_cilindros(request):
         if peca.tipo == "cilindro"
     ]
 
+    user_profile = Profile.objects.filter(user=request.user).first()
+    if user_profile and user_profile.tipo_acesso == "inspetor" and user_profile.permissoes.filter(nome="inspecao/tubos-cilindros").exists():
+        inspetor_logado = {
+            "nome_usuario": request.user.username,
+            "id": request.user.id
+        }
+    else:
+        inspetor_logado = None
+
     return render(
         request,
         "inspecao_tubos_cilindros.html",
@@ -1925,6 +1963,7 @@ def inspecao_tubos_cilindros(request):
             "causas": list_causas,
             "pecas_tubos": list_pecas_tubos,
             "pecas_cilindros": list_pecas_cilindros,
+            "inspetor_logado":inspetor_logado
         },
     )
 
