f3543558d1cca57c6f5f7174422074be980bb462
luan <luan640@gmail.com>
2025-06-03 12:00:40 -0300

    adicionado dentro de admin pecas a opção de filtrar'

--- cadastro/admin.py
+++ cadastro/admin.py
@@ -6,10 +6,22 @@ class PecasEstanqueidadeAdmim(admin.ModelAdmin):
     list_display = ("codigo", "descricao", "tipo")
     list_display_links = ("codigo",)
 
+class PecasAdmin(admin.ModelAdmin):
+    search_fields = ['codigo', 'descricao', 'materia_prima', 'apelido']
+
+    fieldsets = (
+        ('Informações Principais', {
+            'fields': ('codigo', 'descricao', 'materia_prima', 'apelido', 'comprimento')
+        }),
+        ('Relacionamentos', {
+            'fields': ('setor', 'conjunto', 'processo_1')
+        }),
+    )
+
 admin.site.register(Maquina)
 admin.site.register(MotivoInterrupcao)
 admin.site.register(Operador)
-admin.site.register(Pecas)
+admin.site.register(Pecas, PecasAdmin)
 admin.site.register(Setor)
 admin.site.register(Mp)
 admin.site.register(Espessura)
